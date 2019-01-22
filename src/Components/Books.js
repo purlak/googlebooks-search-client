b8f0313a94b300800080671ffecffea69bdab783
github username <purnima.nyc@gmail.com>
2019-01-21 21:15:38 -0500

    display book details - title, author, publisher

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -2,14 +2,22 @@ import React, { Component, Fragment } from 'react'
 
 class Books extends Component {
   render () {
-    console.log(this.props.books)
     if (this.props.books.length === 0) {
 
         return <h2>No books found! Try again!</h2>
     } else {
       return (
         <Fragment>
-          {this.props.books.map(book => <h2>{book.volumeInfo.title}</h2>)}
+          {this.props.books.map(book =>
+            <list>
+              <ul>
+              <h2>{book.volumeInfo.title}</h2>
+              <h3>{book.volumeInfo.authors}</h3>
+              <h3>{book.volumeInfo.publisher}</h3>
+              </ul>
+            </list>
+
+          )}
         </Fragment>
       )
 
