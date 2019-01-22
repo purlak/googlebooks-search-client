f352065f48fc96b6ac363e092b497bea14c50ce9
github username <purnima.nyc@gmail.com>
2019-01-21 20:05:41 -0500

    setup Books component, add conditional to check for no result

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react'
 
 class Books extends Component {
   render (){
-
-    return()
+    if {this.props.books.length === 0} {
+        return <h2>No books found! Try again!</h2>
+    }
   }
-
 }
+
+export default Books;
