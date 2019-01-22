b36e448e86bd4259140ab09a37b6c69e8ac12e1a
github username <purnima.nyc@gmail.com>
2019-01-21 20:52:04 -0500

    display book title

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,9 +1,18 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 class Books extends Component {
-  render (){
-    if {this.props.books.length === 0} {
+  render () {
+    console.log(this.props.books)
+    if (this.props.books.length === 0) {
+
         return <h2>No books found! Try again!</h2>
+    } else {
+      return (
+        <Fragment>
+          {this.props.books.map(book => <h2>{book.volumeInfo.title}</h2>)}
+        </Fragment>
+      )
+
     }
   }
 }
