a6116f5d3a5f6d3122cd309aaafecd0f577ce549
github username <purnima.nyc@gmail.com>
2019-01-22 20:41:36 -0500

    update styling for search results

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import '../Books.css';
 
 class Books extends Component {
@@ -8,23 +8,21 @@ class Books extends Component {
         return <h2>No books found! Try again!</h2>
     } else {
       return (
-        <Fragment>
+        <div>
           {this.props.books.map(book =>
-            <div>
+            <div className="displayBook">
               <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-              <h3 className="displayTitle">{book.volumeInfo.title}</h3>
+              <h3 className="displayTitle">Title: {book.volumeInfo.title}</h3>
               <p className="displayTitle">{book.volumeInfo.authors}</p>
               <p className="displayTitle">{book.volumeInfo.publisher}</p>
-              <a href={`${book.volumeInfo.infoLink}`} target="_blank">More Info</a>
-
+              <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayTitle">More Info</a>
+              <br/>
             </div>
-
           )}
-        </Fragment>
+        </div>
       )
 
     }
   }
 }
-
 export default Books;
