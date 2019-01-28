0345adb436a7df565d2002120281618ae6b21361
github username <purnima.nyc@gmail.com>
2019-01-28 14:02:46 -0500

    clean up code

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import Books from './Components/Books.js'
 
-
-
 const api_url = 'https://www.googleapis.com/books/v1'
 const api_key = process.env.REACT_APP_API_KEY;
 
