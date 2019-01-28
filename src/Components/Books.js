651085ff5f2de807f69a50cd8b9d4d507d3e9297
github username <purnima.nyc@gmail.com>
2019-01-28 15:39:49 -0500

    added checks for book info

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import '../Books.css';
+import No_image_available from '../No_image_available.svg'
 
 class Books extends Component {
   render () {
@@ -10,12 +11,32 @@ class Books extends Component {
         <div>
           {this.props.books.map(book =>
             <div className="displayBook">
-              <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-              <h3 className="displayInfo">{book.volumeInfo.title}</h3>
-              <p className="displayInfo">{book.volumeInfo.authors}</p>
-              <p className="displayInfo">{book.volumeInfo.publisher}</p>
+              {book.volumeInfo.imageLinks.smallThumbnail? (
+                <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
+              ) : (
+                <img src={`${No_image_available}`} className="imageSize"/>
+              )}
+
+              {book.volumeInfo.title? (
+                <h3 className="displayInfo">{book.volumeInfo.title}</h3>
+              ) : (
+                <h3 className="displayInfo">Info N/A</h3>
+              )}
+
+              {book.volumeInfo.authors? (
+                <p className="displayInfo">{book.volumeInfo.authors}</p>
+              ) : (
+                <p>Author Info N/A</p>
+              )}
+
+              {book.volumeInfo.publisher? (
+                <p className="displayInfo">{book.volumeInfo.publisher}</p>
+              ) : (
+                <p>Publisher Info N/A</p>
+              )}
+
               <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
-              <br/>
+            
             </div>
           )}
         </div>
