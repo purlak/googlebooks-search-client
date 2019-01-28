180d3e85e6e50a938e253dff89a3a2b42a3f578b
github username <purnima.nyc@gmail.com>
2019-01-28 15:26:59 -0500

    update styling

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -11,10 +11,10 @@ class Books extends Component {
           {this.props.books.map(book =>
             <div className="displayBook">
               <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-              <h3 className="displayTitle">{book.volumeInfo.title}</h3>
-              <p className="displayTitle">{book.volumeInfo.authors}</p>
-              <p className="displayTitle">{book.volumeInfo.publisher}</p>
-              <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayTitle">More Info</a>
+              <h3 className="displayInfo">{book.volumeInfo.title}</h3>
+              <p className="displayInfo">{book.volumeInfo.authors}</p>
+              <p className="displayInfo">{book.volumeInfo.publisher}</p>
+              <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
               <br/>
             </div>
           )}
