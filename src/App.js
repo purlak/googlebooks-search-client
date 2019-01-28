143d74ad3661d03967fe3384630c44e65f1ad346
github username <purnima.nyc@gmail.com>
2019-01-28 14:53:38 -0500

    bug fix - conditional for no results

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ const api_url = 'https://www.googleapis.com/books/v1'
 const api_key = process.env.REACT_APP_API_KEY;
 
 class App extends Component {
-  constructor () {
-    super ();
+  constructor (props) {
+    super (props);
     // initialize state
     this.state = {
       searchTerm: '',
-      books: []
     };
   }
 
   // event handler to capture search input
   handleSearchInput = event => {
+    event.preventDefault();
     this.setState({searchTerm: event.target.value })
   }
 
   // event handler to fetch search results
-  searchBook = () => {
+  searchBook = (event) => {
     const query = this.state.searchTerm
     fetch (`${api_url}/volumes?q=${query}&key=${api_key}`)
     .then (res => res.json())
@@ -30,6 +30,7 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.books) {
     return (
       <div className="App">
         <header className="App-body">
@@ -39,9 +40,24 @@ class App extends Component {
             <button className="buttonSize" onClick={this.searchBook}>Search</button>
           </div>
           <Books books={this.state.books}/>
+
         </header>
       </div>
     );
+  }
+  else {
+    return (
+      <div className="App">
+        <header className="App-body">
+          <h1 className="welcome">Welcome to the Google Books Search App!</h1>
+          <div>
+            <input className="searchBar" type="text" onChange={this.handleSearchInput} value={this.state.searchTerm} placeholder="search books"/>
+            <button className="buttonSize" onClick={this.searchBook}>Search</button>
+          </div>
+        </header>
+      </div>
+    )
+  }
   }
 }
 
