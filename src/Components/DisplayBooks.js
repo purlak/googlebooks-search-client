14f8d84ca0e5e76333c23475643afb1f01bb4210
github username <purnima.nyc@gmail.com>
2019-01-28 21:24:10 -0500

    update conditionals

--- src/Components/DisplayBooks.js
+++ src/Components/DisplayBooks.js
@@ -5,40 +5,45 @@ import No_image_available from '../css/images/No_image_available.png'
 class Books extends Component {
 
   render () {
-      return (
-        <div>
-          {this.props.books.map(book =>
-            <div className="displayBook">
-              {book.volumeInfo.imageLinks? (
-                <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-              ) : (
-                <img src={`${No_image_available}`} className="imageSize"/>
-              )}
+      if (typeof(this.props.books) === 'undefined'){
+        return (<h2>No books found. Try again.</h2>)
+      }
+      else  if (typeof(this.props.books) !== 'undefined' || this.props.books != null){
+        return (
+          <div>
+            {this.props.books.map(book =>
+              <div className="displayBook">
+                {book.volumeInfo.imageLinks? (
+                  <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
+                ) : (
+                  <img src={`${No_image_available}`} className="imageSize"/>
+                )}
 
-              {book.volumeInfo.title? (
-                <h3 className="displayInfo">{book.volumeInfo.title}</h3>
-              ) : (
-                <h3 className="displayInfo">Info N/A</h3>
-              )}
+                {book.volumeInfo.title? (
+                  <h3 className="displayInfo">{book.volumeInfo.title}</h3>
+                ) : (
+                  <h3 className="displayInfo">Info N/A</h3>
+                )}
 
-              {book.volumeInfo.authors? (
-                <p className="displayInfo">{book.volumeInfo.authors}</p>
-              ) : (
-                <p>Author - N/A</p>
-              )}
+                {book.volumeInfo.authors? (
+                  <p className="displayInfo">{book.volumeInfo.authors}</p>
+                ) : (
+                  <p>Author - N/A</p>
+                )}
 
-              {book.volumeInfo.publisher? (
-                <p className="displayInfo">{book.volumeInfo.publisher}</p>
-              ) : (
-                <p>Publisher - N/A</p>
-              )}
+                {book.volumeInfo.publisher? (
+                  <p className="displayInfo">{book.volumeInfo.publisher}</p>
+                ) : (
+                  <p>Publisher - N/A</p>
+                )}
 
-              <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
+                <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
 
-            </div>
-          )}
-        </div>
-      )}
+              </div>
+            )}
+          </div>
+        )}
+  }
 }
 
 export default Books;
