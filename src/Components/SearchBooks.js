f7dbf99e0f4fc525f73e00fc9ea7dbbef6a3aad1
github username <purnima.nyc@gmail.com>
2019-01-28 22:47:32 -0500

    add event handler for enter key

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -32,7 +32,17 @@ class Search extends Component {
           <header className="App-body">
             <h1 className="welcome">Welcome to the Books Search App!</h1>
             <div>
-              <input className="searchBar" type="text" onChange={this.handleSearchInput} value={this.state.searchTerm} placeholder="search books"/>
+              <input className="searchBar"
+                type="text"
+                onChange={this.handleSearchInput}
+                onKeyPress={event => {
+                  if (event.key === 'Enter') {
+                    this.searchBook()
+                  }
+                }} 
+                value={this.state.searchTerm}
+                placeholder="search books"
+              />
               <button className="buttonSize" onClick={this.searchBook}>Search</button>
             </div>
             <Books books={this.state.books}/>
