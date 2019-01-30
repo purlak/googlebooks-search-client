548037375868fda88c2d7d40aae5c43983df1704
github username <purnima.nyc@gmail.com>
2019-01-29 22:57:58 -0500

    add test

--- src/Components/__tests__/SearchBooks.test.js
+++ src/Components/__tests__/SearchBooks.test.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Search from '../SearchBooks';
 
-describe ('Search', () => {
-  let wrapper;
-
-  beforeEach(() => {
-      wrapper = shallow(<Search />)
-  })
-
-  it ('renders the search bar', () => {
-  })
-
+describe ("Search component", () => {
+    test ("renders", () => {
+        const wrapper = shallow(<Search />);
+        expect(wrapper.exists()).toBe(true);
+    })
 })
+
+
+// describe ('Search', () => {
+//   let wrapper;
+//
+//   beforeEach(() => {
+//       wrapper = shallow(<Search />)
+//   })
+//
+//   it ('renders the search bar', () => {
+//   })
+//
+// })
