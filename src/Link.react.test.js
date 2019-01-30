f1bbf0562b2303c90cbd678fb0a67c1de3b172c8
github username <purnima.nyc@gmail.com>
2019-01-29 19:44:14 -0500

    clean up code

--- src/Link.react.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Search from './Components/SearchBooks';
-import Books from './Components/DisplayBooks';
-import renderer from 'react-test-renderer';
-import {shallow} from 'enzyme';
-
-it('renders Search Component without crashing', () => {
-  shallow(<SearchBooks />);
-});
