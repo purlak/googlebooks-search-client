36b8237b59ab484dd05327a074767e7bc6e89889
github username <purnima.nyc@gmail.com>
2019-02-05 10:14:14 -0500

    update to handle props

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+import BookService from '../Services/BookService.js';
+import '../css/App.css';
 
 class SearchBar extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      searchTerm: '',
+      books: this.props.books
+    };
+  }
+  handleSearchInput = event => {
+    this.setState({searchTerm: event.target.value });
+  }
 
   render () {
+    console.log(this.props.books)
     return (
+      <div>
       <input className="searchBar"
         type="text"
         onChange={this.handleSearchInput}
@@ -16,6 +30,7 @@ class SearchBar extends Component {
         placeholder="search books"
       />
       <button className="buttonSize" onClick={this.searchBook}>Search</button>
+      </div>
     )
   }
 }
