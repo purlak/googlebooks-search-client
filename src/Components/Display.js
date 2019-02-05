6d6cab2df6b1fb1997b95f0f5bbdfec56abe86c8
github username <purnima.nyc@gmail.com>
2019-02-05 15:29:17 -0500

    refactor data display

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,16 +1,40 @@
 import React, { Component } from 'react';
-import DataService from '../Services/DataService'
-import No_image_available from '../css/images/No_image_available.png'
+import DataService from '../Services/DataService';
+import No_image_available from '../css/images/No_image_available.png';
 
 class Display extends Component {
-  render ()   {
-    console.log(this.props.book)
 
-
+  render () {
+    const bookData = this.props.book.volumeInfo;
     return (
-      <h1>test</h1>
-    )
-  }
+      <div>
+      {bookData.imageLinks? (
+        <img src={`${bookData.imageLinks.smallThumbnail}`} className="imageSize"/>
+      ) : (
+        <img src={`${No_image_available}`} className="imageSize"/>
+      )}
+
+      {bookData.title? (
+        <h3 className="displayInfo">Title: {bookData.title}</h3>
+      ) : (
+        <h3 className="displayInfo">Title: Info N/A</h3>
+      )}
+
+      {bookData.authors? (
+        <p className="displayInfo">Author: {bookData.authors[0]}</p>
+      ) : (
+        <p>Author: N/A</p>
+      )}
+
+      {bookData.publisher? (
+        <p className="displayInfo">Publisher: {bookData.publisher}</p>
+      ) : (
+        <p>Publisher: N/A</p>
+      )}
+
+      <a href={`${bookData.infoLink}`} target="_blank" className="displayInfo">More >></a>
+      </div>
+    )}
 }
 
 export default Display;
