94e393915fa4399d4459ec1655e147ac2f3f2496
github username <purnima.nyc@gmail.com>
2019-02-05 09:26:26 -0500

    move searchbar to new component + refactor fetch

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import Books from './DisplayBooks.js';
-import BookService from './Services/BookService.js';
+import BookService from '../Services/BookService.js';
 
-const api_url = 'https://www.googleapis.com/books/v1'
-const api_key = process.env.REACT_APP_API_KEY;
+// const api_url = 'https://www.googleapis.com/books/v1'
+// const api_key = process.env.REACT_APP_API_KEY;
 
 class Search extends Component {
   constructor () {
@@ -15,36 +15,27 @@ class Search extends Component {
   }
 
   componentDidMount() {
-    BookService.fetchBooks().then(books => this.setState({  books: data.items }))
+    BookService.fetchBooks(this.state.searchTerm)
+    .then(data => this.setState({  books: data.items }))
   }
 
   handleSearchInput = event => {
     this.setState({searchTerm: event.target.value });
   }
 
-  searchBook = () => {
-    const query = this.state.searchTerm;
+  // searchBook = () => {
+    // const query = this.state.searchTerm;
+
     // fetch (`${api_url}/volumes?q=${query}&key=${api_key}`)
     // .then (res => res.json())
     // .then(data => this.setState({books: data.items}))
-  }
+  // }
 
   render() {
       return (
         <div>
           <div>
-            <input className="searchBar"
-              type="text"
-              onChange={this.handleSearchInput}
-              onKeyPress={event => {
-                if (event.key === 'Enter') {
-                  this.searchBook()
-                }
-              }}
-              value={this.state.searchTerm}
-              placeholder="search books"
-            />
-            <button className="buttonSize" onClick={this.searchBook}>Search</button>
+            <SearchBar />
           </div>
           <Books books={this.state.books}/>
         </div>
