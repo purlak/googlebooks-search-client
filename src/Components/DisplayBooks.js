299dbc9f3e8dbecf66942a043c8bfbc0afccafaa
github username <purnima.nyc@gmail.com>
2019-02-05 12:36:43 -0500

    setup new display component

--- src/Components/DisplayBooks.js
+++ src/Components/DisplayBooks.js
@@ -13,33 +13,8 @@ class Books extends Component {
           <div>
             {this.props.books.map(book =>
               <div className="displayBook">
-                {book.volumeInfo.imageLinks? (
-                  <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
-                ) : (
-                  <img src={`${No_image_available}`} className="imageSize"/>
-                )}
-
-                {book.volumeInfo.title? (
-                  <h3 className="displayInfo">Title: {book.volumeInfo.title}</h3>
-                ) : (
-                  <h3 className="displayInfo">Title: Info N/A</h3>
-                )}
-
-                {book.volumeInfo.authors? (
-                  <p className="displayInfo">Author: {book.volumeInfo.authors[0]}</p>
-                ) : (
-                  <p>Author: N/A</p>
-                )}
-
-                {book.volumeInfo.publisher? (
-                  <p className="displayInfo">Publisher: {book.volumeInfo.publisher}</p>
-                ) : (
-                  <p>Publisher: N/A</p>
-                )}
-
-                <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
-
-              </div>
+                  <Display />
+              <div>    
             )}
           </div>
         )}
