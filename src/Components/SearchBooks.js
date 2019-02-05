ca292902cb8ea8c1a0bb70350fa77d2318222fef
github username <purnima.nyc@gmail.com>
2019-02-05 11:26:47 -0500

    clean up code

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar.js';
 import Books from './DisplayBooks.js';
 
-// const api_url = 'https://www.googleapis.com/books/v1'
-// const api_key = process.env.REACT_APP_API_KEY;
-
 class Search extends Component {
   constructor () {
     super ();
