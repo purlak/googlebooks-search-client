738f25f146f0223fddd397dcb33d07b1e82052af
github username <purnima.nyc@gmail.com>
2019-02-05 21:04:41 -0500

    add react-detect-offline tool

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './css/App.css';
 import Search from './Components/SearchBooks.js';
+import { Offline, Online } from "react-detect-offline";
 
 class App extends Component {
   render() {
@@ -8,7 +9,12 @@ class App extends Component {
       <div className="App">
         <div className="App-body">
           <h1 className="welcome">Welcome to the Books Search App!</h1>
-          <Search />
+          <Online>
+            <Search />
+          </Online>
+          <Offline>
+            You're offline right now. Check your connection.
+          </Offline>
         </div>
       </div>
     )
