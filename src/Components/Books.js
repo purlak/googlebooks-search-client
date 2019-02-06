cfc3cb4a21b4cc246b05c969277fc0a0ae85cd62
github username <purnima.nyc@gmail.com>
2019-02-06 18:29:50 -0500

    setup new function to check validity of attribute

--- src/Components/Books.js
+++ src/Components/Books.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
 import DisplayBooks from './DisplayBooks.js'
 import '../css/DisplayBooks.css';
+import No_image_available from '../css/images/No_image_available.png';
 
 class Books extends Component {
+
+  constructor(props) {
+    super(props);
+    this.getInfoOrDisplayDefault = this.getInfoOrDisplayDefault.bind(this);
+  }
+
+  getInfoOrDisplayDefault(data) {
+    Object.keys(data.volumeInfo).map(function(key) {
+      console.log(key)
+      switch (key)  {
+        case 'title':
+          console.log(data.volumeInfo[key])
+          default:
+            console.log('foo');
+      }
+
+      }
+
+    )
+    }
+
 
   render () {
       if (typeof(this.props.books) === 'undefined'){
@@ -11,13 +33,47 @@ class Books extends Component {
       else if (typeof(this.props.books) !== 'undefined' || this.props.books !== null){
         return (
           <div>
-            {this.props.books.map(book =>
-              <div className="displayBook">
-                <Display book={book}/>
-              </div>
-            )}
-          </div>
+          {this.props.books.map(book =>
+            <div className="displayBook">
+              {this.getInfoOrDisplayDefault(book)}
+            </div>
         )}
-    }
+        </div>
+    )}
+  }
 }
 export default Books;
+
+
+
+// {this.props.books.map(book =>
+//   <div className="displayBook">
+//     <Display book={book}/>
+//   </div>
+// )}
+
+// {book.volumeInfo.imageLinks? (
+//   <img src={`${book.volumeInfo.imageLinks.smallThumbnail}`} className="imageSize"/>
+// ) : (
+//   <img src={`${No_image_available}`} className="imageSize"/>
+// )}
+//
+// {book.volumeInfo.title? (
+//   <h3 className="displayInfo">Title: {book.volumeInfo.title}</h3>
+// ) : (
+//   <h3 className="displayInfo">Title: Info N/A</h3>
+// )}
+//
+// {book.volumeInfo.authors? (
+//   <p className="displayInfo">Author: {book.volumeInfo.authors[0]}</p>
+// ) : (
+//   <p>Author: N/A</p>
+// )}
+//
+// {book.volumeInfo.publisher? (
+//   <p className="displayInfo">Publisher: {book.volumeInfo.publisher}</p>
+// ) : (
+//   <p>Publisher: N/A</p>
+// )}
+//
+// <a href={`${book.volumeInfo.infoLink}`} target="_blank" className="displayInfo">More >></a>
