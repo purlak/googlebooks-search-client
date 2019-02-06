e719c92b095525684bd4b8c7de5301214898fa0a
github username <purnima.nyc@gmail.com>
2019-02-06 15:35:12 -0500

    refactor duplicate code

---
+++ src/Components/DisplayBooks.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import DataService from '../Services/DataService';
+import '../css/DisplayBooks.css';
+
+import No_image_available from '../css/images/No_image_available.png';
+
+class Display extends Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+    //   data: {
+    //   'title': '',
+    //   'authors': '',
+    //   'publisher': '',
+    //   'imageLinks': ''
+    // } ,
+      bookData: this.props.book.volumeInfo
+    };
+  }
+
+  render () {
+    const display = DataService.checkData(this.state.bookData)
+    return (
+      <div className="displayInfo">
+        {display}
+      </div>
+    )}
+}
+
+export default Display;
+
+// {bookData.title? (
+//   <h3 className="displayInfo">Title: {bookData.title}</h3>
+// ) : (
+//   <h3 className="displayInfo">Title: Info N/A</h3>
+// )}
+// {bookData.imageLinks? (
+//   <img src={`${bookData.imageLinks.smallThumbnail}`} className="imageSize"/>
+// ) : (
+//   <img src={`${No_image_available}`} className="imageSize"/>
+// )}
+// <p></p>
+// {bookData.authors? (
+//   <p className="displayInfo">Author: {bookData.authors[0]}</p>
+// ) : (
+//   <p>Author: N/A</p>
+// )}
+//
+// {bookData.publisher? (
+//   <p className="displayInfo">Publisher: {bookData.publisher}</p>
+// ) : (
+//   <p>Publisher: N/A</p>
+// )}
+//
+// <a href={`${bookData.infoLink}`} target="_blank" className="displayInfo">More >></a>
