7ef7fd8f4ee91bace21f74382e6425feaf478c68
github username <purnima.nyc@gmail.com>
2019-02-05 22:18:42 -0500

    add error handling for rejected requests

--- src/Services/BookService.js
+++ src/Services/BookService.js
@@ -4,7 +4,14 @@ const api_key = process.env.REACT_APP_API_KEY;
 const BookService = {
   fetchBooks (query) {
     return fetch (`${api_url}/volumes?q=${query}&key=${api_key}`)
-    .then (res => res.json())
+    .then (res => {
+      if (res.ok) {
+        return res.json()
+      }
+      else {
+        throw Error(`Request rejected with status ${res.status}`);
+      }
+    })
   }
 }
 
