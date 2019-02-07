0c8f6ddd81d0217280f91ed843f27e5fab1f7767
github username <purnima.nyc@gmail.com>
2019-02-07 13:33:01 -0500

    add new function to check data

---
+++ src/Services/DataService.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../css/DisplayBooks.css'
+const DataService = {
+  searchBook(data) {
+    console.log(data)
+    switch (typeof(data)) {
+        case 'string':
+          if ((data).includes("&source=gbs_api")) {
+            return (
+            <div>
+            <a href={`${data}`} target="_blank" className="displayInfo">More Info>> </a>
+            </div>
+            )
+            break;
+          } else {
+          return (<h2 className="displayInfo">{data}</h2>)
+          break;
+        }
+        case 'object':
+          if (data instanceof Array) {
+            return (<h2 className="displayInfo">{data[0]}</h2>)
+            break;
+          }
+          else {
+            return <img src={`${data.smallThumbnail}`} className="imageSize"/>
+            break;
+          }
+      }
+  }
+}
+
+export default DataService;
