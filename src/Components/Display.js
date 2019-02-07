32f658df0e4af3c831897f896b3215ff257d0501
github username <purnima.nyc@gmail.com>
2019-02-07 13:35:00 -0500

    clean up code

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import DisplayBooks from './DisplayBooks.js';
-import DisplayImage from './DisplayImage.js';
-import DisplayInfoLink from './DisplayInfoLink.js';
+import DataService from '../Services/DataService.js'
 
 import '../css/DisplayBooks.css';
 
@@ -9,40 +7,24 @@ class Display extends Component {
 
   render () {
     const data = this.props.book.volumeInfo
-    const key1 = ['title', 'authors', 'publisher']
-    const key2 = ['imageLinks']
-    const key3 = ['infoLink']
-
+    const keys = ['title', 'authors', 'publisher', 'imageLinks', 'infoLink']
     return (
-
-    Object.keys(this.props.book.volumeInfo).map(function(key) {
-      if (key2.includes(key) && data[key]) {
-        return (
-          <DisplayImage data={data[key]}/>
-      )}
-
-      else if (key1.includes(key) && data[key]) {
-        return (
-          <DisplayBooks data={data[key]} />
-        )
-      }
-
-      else if (key3.includes(key) && data[key]) {
-        return (
-          <DisplayInfoLink data={data[key]} />
-        )
-      }
-
-      else if (key1.includes(key) || key2.includes(key)) return (
-        <div>
-          <p className="displayInfo">{ key }: N/A</p>
-        </div>
-      )
-    })
-
-  )
+      Object.keys(this.props.book.volumeInfo).map(function(key) {
+        if (keys.includes(key) && (data[key])) {
+          return (
+            <div>
+              {DataService.searchBook(data[key])}
+            </div>
+          )}
+        else if (keys.includes(key)) {
+          return (
+            <div>
+              <p className="displayInfo">{ key }: N/A</p>
+            </div>
+        )}
+      })
+    )
   }
-
 }
 
 export default Display;
