f651fd3b35fe35808f566b0b6f938296e84624c2
github username <purnima.nyc@gmail.com>
2019-02-07 20:41:08 -0500

    refactored - used alternate method

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -4,26 +4,42 @@ import '../css/DisplayBooks.css';
 
 class Display extends Component {
 
+  getOrDefault = (key, volumeInfo) => {
+    if (volumeInfo[key]) {
+        return DataService.searchBook(volumeInfo[key])
+    }
+    return (
+          <p className="displayInfo">{ key }: N/A</p>
+    )
+  }
+
   render () {
     const data = this.props.book.volumeInfo
     const keys = ['title', 'authors', 'publisher', 'imageLinks', 'infoLink']
     return (
-      Object.keys(this.props.book.volumeInfo).map(function(key) {
-        if (keys.includes(key) && (data[key])) {
-          return (
-            <div>
-              {DataService.searchBook(data[key])}
-            </div>
-          )}
-        else if (keys.includes(key) && !(data[key])){
-          return (
-            <div>
-              <p className="displayInfo">{ key }: N/A</p>
-            </div>
-        )}
+      keys.map((key) => {
+        return (
+          <div>{ this.getOrDefault(key, volumeInfo) }</div>
+        )
       })
     )
   }
 }
 
 export default Display;
+
+// Alternate code
+// Object.keys(this.props.book.volumeInfo).map(function(key) {
+//   if (keys.includes(key) && (data[key])) {
+//     return (
+//       <div>
+//         {DataService.searchBook(data[key])}
+//       </div>
+//     )}
+//   else if (keys.includes(key) && !(data[key])){
+//     return (
+//       <div>
+//         <p className="displayInfo">{ key }: N/A</p>
+//       </div>
+//   )}
+// })
