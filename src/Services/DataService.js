6388cb5b467138aee01a03b594f96bfd363b448d
github username <purnima.nyc@gmail.com>
2019-02-07 21:11:16 -0500

    remove alternate code

--- src/Services/DataService.js
+++ src/Services/DataService.js
@@ -33,32 +33,4 @@ const DataService = {
       }
   }
 }
-
 export default DataService;
-
-// alternate code
-// switch (typeof(data)) {
-//     case 'string':
-//       if ((data).includes("&source=gbs_api")) {
-//         return (
-//         <div>
-//         <a href={`${data}`} target="_blank" className="displayInfo">More Info>> </a>
-//         </div>
-//         )
-//         break;
-//       } else {
-//       return (<h2 className="displayInfo">{data}</h2>)
-//       break;
-//     }
-//     case 'object':
-//       if (data instanceof Array) {
-//         return (<h2 className="displayInfo">{data[0]}</h2>)
-//         break;
-//       }
-//       else {
-//         return <img src={`${data.smallThumbnail}`} className="imageSize"/>
-//         break;
-//     default:
-//       return;
-//       }
-//   }
